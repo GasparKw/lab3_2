ebaff37525638cd1d52532646812a5a5c2db0ce9
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-04-27 12:37:39 +0200

    Stworzono klase testowa

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -2,6 +2,9 @@ package edu.iis.mto.staticmock;
 
 import static org.junit.Assert.*;
 
+import edu.iis.mto.staticmock.reader.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -12,13 +15,18 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(NewsLoader.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
+
 
 public class NewsLoaderTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	@Before
+	public void setUp() {
+		
 	}
 
+	@Test
+	public void test_loadNews() {
+		
+	}
 }
